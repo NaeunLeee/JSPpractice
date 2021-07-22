def1bef0c055374eb3c58566cc0770ac17694c2b
NaeunLeee <nue.an.2@gmail.com>
2021-07-23 08:56:25 +0900

    JSP practcie

---
+++ ServletTest/src/main/java/co/micol/servletTest/HelloWorld.java
@@ -0,0 +1,49 @@
+package co.micol.servletTest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Get 방식으로 호출");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		out.print("<h1> 서블릿에서 호출한 내용입니다. </h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Post 방식으로 호출");
+		doGet(request, response);
+	}
+
+}
---
+++ ServletTest/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>여기가 시작이야!!!</h1>
+<h1><a href="HelloWorld?id=hong">서블릿 호출</a></h1>
+
+<form id="frm" name="frm" method="post" action="HelloWorld">
+	<input type="submit" value="전송">
+</form>
+
+</body>
+</html>
