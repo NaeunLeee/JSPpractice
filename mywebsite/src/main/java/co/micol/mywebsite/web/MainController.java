554698a0f2827808b4acec3d63498adc34f9fee5
NaeunLeee <nue.an.2@gmail.com>
2021-07-29 23:02:42 +0900

    mywebsite test

---
+++ mywebsite/src/main/java/co/micol/mywebsite/web/MainController.java
@@ -0,0 +1,33 @@
+package co.micol.mywebsite.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/main.do")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MainController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String page = "member/memberList.tiles";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		dispatcher.forward(request, response);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ mywebsite/src/main/webapp/WEB-INF/views/member/memberList.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div align="center">
+		<div><h1>회원 리스트</h1></div>
+		<div><h1>여기 회원정보 나옴</h1></div>
+	</div>
+</body>
+</html>
